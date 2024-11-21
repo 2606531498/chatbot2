27bdc41fab7492f8b758e0ae9d1bf85079335ff8
liuqian <2606531498@qq.com>
2024-11-21 17:36:04 +0800

    Fix: 修复连接问题

--- next.config.js
+++ next.config.js
@@ -2,8 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
-  poweredByHeader: false,
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
+  },
   async headers() {
     return [
       {
